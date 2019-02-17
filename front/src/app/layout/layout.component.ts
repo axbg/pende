4e9a486aa9a9c83cef05efbf4a84bdae2bede0ab
axbg <bisagalexstefan@gmail.com>
2019-02-17 23:54:17 +0200

    Implemented tab-ribbon delete navigation, but noticed a little bug mentioned as a commentary.

--- front/src/app/layout/layout.component.ts
+++ front/src/app/layout/layout.component.ts
@@ -20,15 +20,15 @@ export class LayoutComponent implements OnInit {
   }
 
   initFileTabs(){
-    this.fileTabs.push(new NavigationTab("test1", "url1", "code-editor", "content1"));
-    this.fileTabs.push(new NavigationTab("test2", "url2", "code-editor", "content2"));
-    this.fileTabs.push(new NavigationTab("test3", "url3", "code-editor", "content3"));
-    this.fileTabs.push(new NavigationTab("test4", "url3", "code-editor", "content4"));
+    this.fileTabs.push(new NavigationTab("test1", "url1", "content1"));
+    this.fileTabs.push(new NavigationTab("test2", "url2", "content2"));
+    this.fileTabs.push(new NavigationTab("test3", "url3", "content3"));
+    this.fileTabs.push(new NavigationTab("test4", "url3", "content4"));
   }
 
   initprojectTabs(){
-    this.projectTabs.push(new NavigationTab("test1", "url1", "some div", "content1"));
-    this.projectTabs.push(new NavigationTab("test2", "url2", "some div", "content2"));
+    this.projectTabs.push(new NavigationTab("test1", "url1", "content1"));
+    this.projectTabs.push(new NavigationTab("test2", "url2", "content2"));
   }
 
 }
