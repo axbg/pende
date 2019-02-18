ba46f4bcc314bd5fcd7bc96eed02b55b3324dc21
axbg <bisagalexstefan@gmail.com>
2019-02-19 01:57:11 +0200

    Integrated ng2-ace-editor. Created AceEditorComponent. Created TabEditingServiceService. Implemented data flow between AceEditorComponent and selected TabRibbon element.

---
+++ front/src/app/ace-editor/ace-editor.component.ts
@@ -0,0 +1,56 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { TabEditingServiceService } from '../tab-editing-service.service';
+import { NavigationTab } from '../../classes/NavigationTab';
+import { of } from 'rxjs';
+
+@Component({
+  selector: 'app-ace-editor',
+  templateUrl: './ace-editor.component.html',
+  styleUrls: ['./ace-editor.component.css'],
+})
+export class AceEditorComponent implements OnInit {
+  @ViewChild('editor') editor;
+  currentTab: NavigationTab;
+
+  constructor(private tabEditingService: TabEditingServiceService) {
+    this.tabEditingService.tabOpened$.subscribe(
+      tab => {
+
+        if (this.currentTab) {
+          this.currentTab.setContent(this.editor.getEditor().getValue());
+          this.tabEditingService.saveTabSource(this.currentTab);
+          //sends the new cursor position
+        }
+
+        this.currentTab = tab;
+        this.editor.getEditor().setValue(this.currentTab.getContent());
+        //get the cursor from object and position it
+      }
+    )
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    this.editor.setTheme("dracula");
+    this.editor.setMode("c_cpp");
+    this.editor.getEditor().setValue("//Happy Hacking");
+
+    this.editor.getEditor().setOptions({
+      enableLiveAutocompletion: true,
+      copyWithEmptySelection: true
+    });
+
+    this.editor.getEditor().commands.addCommand({
+      name: "showOtherCompletions",
+      bindKey: "Ctrl-.",
+      exec: function (editor) {
+        console.log(editor.getValue());
+      }
+    })
+  }
+
+
+}
