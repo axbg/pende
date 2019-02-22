bd17c730626380810465dc626c4e8367ec5d44b1
axbg <bisagalexstefan@gmail.com>
2019-02-22 22:49:54 +0200

    Implemented PanelHolder logic for displaying different panels based on the panel property modified using a service. Removed ControlTab component.

--- front/src/app/tab-editing-service.service.ts
+++ front/src/app/tab-editing-service.service.ts
@@ -9,16 +9,22 @@ export class TabEditingServiceService {
 
   private currentFileSource = new Subject<NavigationTab>();
   private modifiedFileSource = new Subject<NavigationTab>();
+  private modifiedMenuPanel = new Subject<String>();
 
   tabOpened$ = this.currentFileSource.asObservable();
   tabClosed$ = this.modifiedFileSource.asObservable();
+  menuPanel$ = this.modifiedMenuPanel.asObservable();
 
-  renderTabSource(tab : NavigationTab){
+  renderTabSource(tab: NavigationTab) {
     this.currentFileSource.next(tab);
   }
 
-  saveTabSource(tab : NavigationTab){
+  saveTabSource(tab: NavigationTab) {
     this.modifiedFileSource.next(tab);
+  }
+
+  renderMenuPanel(panel: String) {
+    this.modifiedMenuPanel.next(panel);
   }
 
 }
