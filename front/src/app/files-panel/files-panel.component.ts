b075924438e12532fba6e97b24b76e152ff21609
axbg <bisagalexstefan@gmail.com>
2019-03-05 23:59:10 +0200

    Implemented ng2-tree in FilesPanelComponent.

--- front/src/app/files-panel/files-panel.component.ts
+++ front/src/app/files-panel/files-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TreeModel, Ng2TreeSettings } from 'ng2-tree';
 
 @Component({
   selector: 'app-files-panel',
@@ -7,9 +8,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilesPanelComponent implements OnInit {
 
-  constructor() { }
+  private dynamicChildrenExample = [
+    { value: 'first_children', id: 1 },
+    {
+      value: 'second_children', range: 3, children: [
+        {
+          value: 'asd'
+        }
+      ]
+    }
+  ];
+
+  private tree: TreeModel = {
+    value: 'projects',
+    id: 0,
+    settings: {
+      'static': false,
+      'rightMenu': true,
+      'leftMenu': false,
+      'cssClasses': {
+        'expanded': 'pi pi-angle-down margin clickable',
+        'collapsed': 'pi pi-angle-right margin clickable',
+        'leaf': 'nothing',
+        'empty': 'pi pi-circle-off margin clickable'
+      },
+      'templates': {
+        'node': '<i class="pi pi-folder clickable"></i>',
+        'leaf': '<i class="pi pi-file clickable"></i>',
+        'leftMenu': '<i></i>',
+      }
+    }
+  }
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+    this.tree.children = this.dynamicChildrenExample;
+  }
+
+  public handleSelected(event) {
+    console.log("selected " + event.node.value);
+  }
+
+  public handleRemoved(event) {
+    let currentNode = event.node;
+    if (confirm("Do you want to delete this element?")) {
+      console.log('deleted ' + event.node.value);
+    } else {
+      currentNode.parent.addChild(currentNode);
+    }
+  }
+
+  public handleRename(event) {
+    console.log("want to rename");
+  }
+
+  public handleCreate(event) {
+    if (event.node.children) {
+      console.log("created new folder with parent: " + event.node.parent.value);
+    } else {
+      console.log("created new file with parent: " + event.node.parent.value);
+    }
+  }
+
+  public handleMoved(event){
+    console.log('moved');
   }
 
 }
