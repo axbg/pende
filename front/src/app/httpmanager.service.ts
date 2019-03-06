d2f8d3fd89303875239c4a9fcebfe9d4933ac55b
axbg <bisagalexstefan@gmail.com>
2019-03-06 08:48:56 +0200

    Created Constants class. Created HTTPManager.

---
+++ front/src/app/httpmanager.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Constants } from '../classes/Constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HTTPManagerService {
+
+  constructor(private http: HttpClient) { }
+
+
+  public apiCall(URL: String, METHOD: String, BODY?: any) {
+    let body;
+
+    if (BODY) {
+      body = new HttpParams({ fromObject: BODY });
+    }
+
+    switch (METHOD) {
+      case Constants.GET_METHOD:
+        return this.http.get(Constants.BASE_URL + URL);
+      case Constants.POST_METHOD:
+        return this.http.post(Constants.BASE_URL + URL, { body: body });
+      case Constants.PUT_METHOD:
+        return this.http.put(Constants.BASE_URL + URL, { body: body });
+      case Constants.DELETE_METHOD:
+        return this.http.delete(Constants.BASE_URL + URL, { body: body });
+    }
+  }
+
+}
