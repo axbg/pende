97e8bc96ead4efe8a4dd51bfc06e8633f934ff2f
axbg <bisagalexstefan@gmail.com>
2019-03-06 08:48:56 +0200

    Created Constants class. Created HTTPManager.

---
+++ front/src/classes/Constants.ts
@@ -0,0 +1,7 @@
+export class Constants {
+    public static readonly BASE_URL = "";
+    public static readonly GET_METHOD = "GET";
+    public static readonly POST_METHOD = "POST";
+    public static readonly PUT_METHOD = "PUT";
+    public static readonly DELETE_METHOD = "DELETE";
+}
