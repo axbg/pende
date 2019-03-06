849bff3b6125799ba59a6c1c4e297d3431673e3c
axbg <bisagalexstefan@gmail.com>
2019-03-06 08:48:56 +0200

    Created Constants class. Created HTTPManager.

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
@@ -53,7 +54,8 @@ import { TreeModule } from 'ng2-tree'
     SliderModule,
     InputSwitchModule,
     TooltipModule,
-    TreeModule
+    TreeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
