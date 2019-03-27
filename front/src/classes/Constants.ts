24873cd3dea3434b040d18dda33fa2bcfdfddf3c
axbg <bisagalexstefan@gmail.com>
2019-03-27 21:58:38 +0200

    Back-end init. Removed front.

--- front/src/classes/Constants.ts
+++
@@ -1,9 +0,0 @@
-export class Constants {
-    public static readonly BASE_URL: string = "";
-    public static readonly GET_METHOD: string = "GET";
-    public static readonly POST_METHOD: string = "POST";
-    public static readonly PUT_METHOD: string = "PUT";
-    public static readonly DELETE_METHOD: string = "DELETE";
-    public static readonly WHITE_THEMES: String[] = ["eclipse", "chrome"];
-    public static readonly BLACK_THEMES: String[] = ["twilight", "dracula", "ambiance", "cobalt"];
-}
