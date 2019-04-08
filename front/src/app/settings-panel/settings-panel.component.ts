d76e6e879f0a7c525558c6360b2841d454a8c9b1
Alex Bisag <bisagalexstefan@gmail.com>
2019-04-09 01:09:19 +0300

    Setbuf error correction. Dynamic settings save button status.

--- front/src/app/settings-panel/settings-panel.component.ts
+++ front/src/app/settings-panel/settings-panel.component.ts
@@ -50,10 +50,12 @@ export class SettingsPanelComponent implements OnInit {
   }
 
   handlePropertyChange(event) {
+    (<HTMLElement>document.querySelector(".ui-button")).style.backgroundColor = "#B71C1C";
     this.settingsService.modifySettings(event.value);
   }
 
   saveSettings(){
+    (<HTMLElement>document.querySelector(".ui-button")).style.backgroundColor = "#007AD9";
     this.settingsService.saveCurrentSettings();
   }
 }
