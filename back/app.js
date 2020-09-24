c0657be3f691ac8cbfde04422880622b8d1c7f5a
axbg <bisagalexstefan@gmail.com>
2020-09-24 09:13:29 +0300

    Added logging

--- back/app.js
+++ back/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
-const fs = require('fs');
+const winston = require('./logger');
+const process = require('process');
+const cors = require('cors');
 
 const PORT = require('./config').PORT;
 const router = require('./routes/index');
@@ -14,9 +15,7 @@ const wsController = require('./controllers/ws').handleWS;
 app.use(cors());
 app.use(express.json());
 
-app.use(
-    morgan('combined', {stream: fs.createWriteStream('./logs', {flags: 'a'})}),
-);
+app.use(morgan('combined', {stream: winston.stream}));
 
 app.get('/', (_, res) => {
   res.send({message: 'pende back-end'});
@@ -25,6 +24,18 @@ app.get('/', (_, res) => {
 io.on('connection', wsController);
 
 app.use('/api', router);
+
+app.use((err, req, res, next) => {
+  winston.error(err.stack);
+
+  res
+      .status(err.status || 500)
+      .send({message: err.message || 'Unknown error occurred'});
+});
+
+process.on('uncaughtException', (err) => {
+  winston.error(err.stack);
+});
 
 server.listen(PORT, () => {
   console.log('pende-back started on http://localhost:' + PORT);
